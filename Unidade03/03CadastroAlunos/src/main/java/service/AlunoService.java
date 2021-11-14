736a179d2ff37204ed24241803c9565690af5211
David Freitas Marzola <77236515+davidfreitasm@users.noreply.github.com>
2021-11-14 09:00:04 -0300

    Integração Front-End com Back-End
    Criei um CRUD similar ao que está na Unidade02 , contudo, fiz leitura de
    dados usando um formulário html. Com o uso de Eclipse, Maven e Spark Framework.

---
+++ Unidade03/03CadastroAlunos/src/main/java/service/AlunoService.java
@@ -0,0 +1,120 @@
+package service;
+
+import java.io.IOException;
+
+import dao.AlunoDAO;
+import model.Aluno;
+import spark.Request;
+import spark.Response;
+public class AlunoService {
+
+	private AlunoDAO AlunoDAO;
+
+	public AlunoService() {
+		//acesso fake ao banco de dados
+		try {
+			AlunoDAO = new AlunoDAO("aluno.dat");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+            e.printStackTrace();
+		}
+	}
+
+	public Object add(Request request, Response response) {
+			int matricula = Integer.parseInt(request.queryParams("matricula"));
+			String nome = request.queryParams("nome");
+			String email = request.queryParams("email");
+			String curso = request.queryParams("curso");
+			int telefone = Integer.parseInt(request.queryParams("telefone"));
+
+			int id = AlunoDAO.getMaxId() + 1;
+
+			Aluno aluno = new Aluno(id, matricula, nome, email, curso, telefone);
+
+			AlunoDAO.add(aluno);
+
+			response.status(201); // 201 Created
+			return id;
+	}
+
+	public Object get(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+		
+		Aluno aluno = (Aluno) AlunoDAO.get(id);
+		
+		if (aluno != null) {
+    	    response.header("Content-Type", "application/xml");
+    	    response.header("Content-Encoding", "UTF-8");
+
+            return "<aluno>\n" + 
+                    "\t<id>" + aluno.getId() + "</id>\n" +
+            		"\t<matricula>" + aluno.getMatricula() + "</matricula>\n" +
+            		"\t<nome>" + aluno.getNome() + "</nome>\n" +
+            		"\t<email>" + aluno.getEmail() + "</email>\n" +
+            		"\t<curso>" + aluno.getCurso() + "</curso>\n" +
+            		"\t<telefone>" + aluno.getTelefone() + "</telefone>\n" +
+            		"</aluno>\n";
+        } else {
+            response.status(404); // 404 Not found
+            return "Aluno " + id + " não encontrado.";
+        }
+
+	}
+
+	public Object update(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+        
+		Aluno aluno = (Aluno) AlunoDAO.get(id);
+
+        if (aluno != null) {
+        	aluno.setMatricula(Integer.parseInt(request.queryParams("matricula")));
+        	aluno.setNome(request.queryParams("nome"));
+        	aluno.setEmail(request.queryParams("email"));
+        	aluno.setCurso(request.queryParams("curso"));
+        	aluno.setTelefone(Integer.parseInt(request.queryParams("telefone")));
+
+        	AlunoDAO.update(aluno);
+        	
+            return id;
+        } else {
+            response.status(404); // 404 Not found
+            return "Aluno não encontrado.";
+        }
+
+	}
+
+	public Object remove(Request request, Response response) {
+		int id = Integer.parseInt(request.params(":id"));
+
+        Aluno aluno = (Aluno) AlunoDAO.get(id);
+
+        if (aluno != null) {
+
+            AlunoDAO.remove(aluno);
+
+            response.status(200); // success
+        	return id;
+        } else {
+            response.status(404); // 404 Not found
+            return "Aluno não encontrado.";
+        }
+	}
+
+	public Object getAll(Request request, Response response) {
+		StringBuffer returnValue = new StringBuffer("<alunos type=\"array\">");
+		for (Aluno aluno : AlunoDAO.getAll()) {
+			returnValue.append("\n<aluno>\n" + 
+					"\t<id>" + aluno.getId() + "</id>\n" +
+            		"\t<matricula>" + aluno.getMatricula() + "</matricula>\n" +
+            		"\t<nome>" + aluno.getNome() + "</nome>\n" +
+            		"\t<email>" + aluno.getEmail() + "</email>\n" +
+            		"\t<curso>" + aluno.getCurso() + "</curso>\n" +
+            		"\t<telefone>" + aluno.getTelefone() + "</telefone>\n" +
+            		"</aluno>\n");
+		}
+		returnValue.append("</alunos>");
+	    response.header("Content-Type", "application/xml");
+	    response.header("Content-Encoding", "UTF-8");
+		return returnValue.toString();
+	}
+}
